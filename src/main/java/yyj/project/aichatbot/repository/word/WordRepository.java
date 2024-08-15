77555570efb61c3c8bdcea9049408e338fc4affd
유 영진 <108460833+YuYeongjin@users.noreply.github.com>
2024-08-15 12:29:42 +0900

    Update WordRepository.java

--- src/main/java/yyj/project/aichatbot/repository/word/WordRepository.java
+++ src/main/java/yyj/project/aichatbot/repository/word/WordRepository.java
@@ -1,6 +1,9 @@
 package yyj.project.aichatbot.repository.word;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import yyj.project.aichatbot.model.Word;
 
 public interface WordRepository extends JpaRepository<Word,Long> {
@@ -9,5 +12,11 @@ public interface WordRepository extends JpaRepository<Word,Long> {
 
     void deleteByWord(String word);
     void deleteByMean(String mean);
+    @Modifying
+    @Query("UPDATE Word w SET w.word = :word WHERE w.id = :id")
+    void updateWordById(@Param("id") Long id, @Param("word") String word);
 
+    @Modifying
+    @Query("UPDATE Word w SET w.mean = :mean WHERE w.id = :id")
+    void updateMeanById(@Param("id") Long id, @Param("mean") String mean);
 }
